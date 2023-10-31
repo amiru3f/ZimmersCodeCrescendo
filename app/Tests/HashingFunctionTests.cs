67e3de875800177de1ac82d3a00fc11e30154aca
amiru3f <solhi.amir1371@gmail.com>
2023-10-31 22:29:15 +0100

    + some tests cases + native cpp test

--- app/Tests/HashingFunctionTests.cs
+++ app/Tests/HashingFunctionTests.cs
@@ -6,15 +6,18 @@ namespace App.Tests;
 
 public class HashingFunctionTests
 {
-    private const int Iterations = 10000;
     private const int OutputLength = 64;
     private const int SaltSize = 16;
+
     private readonly HashAlgorithmName HashAlgorithmName = HashAlgorithmName.SHA512;
 
     [Theory]
-    [InlineData("admin")]
-    [InlineData("A1b@C#d$e^")]
-    public void GivenAHashedPasswordGeneratedWithLegacyMethod_MustBeVerifiedWithBCLStaticMethod(string password)
+    [InlineData("admin", 100)]
+    [InlineData("admin", 10000)]
+    [InlineData("A1b@C#d$e^", 10000)]
+    [InlineData("A1b@C#d$e^", 100)]
+    [InlineData("A1b@C#d$e^", 1)]
+    public void GivenAHashedPasswordGeneratedWithLegacyMethod_MustBeVerifiedWithBCLStaticMethod(string password, int iterations)
     {
         byte[] saltBytes;
         byte[] bytesToVerify;
@@ -22,7 +25,7 @@ public class HashingFunctionTests
         using (var algorithm = new Rfc2898DeriveBytes(
              password,
              SaltSize,
-             Iterations,
+             iterations,
              HashAlgorithmName))
         {
             bytesToVerify = algorithm.GetBytes(OutputLength);
@@ -30,8 +33,47 @@ public class HashingFunctionTests
         }
 
         byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-        var result = Rfc2898DeriveBytes.Pbkdf2(passwordBytes, saltBytes, Iterations, HashAlgorithmName, OutputLength);
+        var result = Rfc2898DeriveBytes.Pbkdf2(passwordBytes, saltBytes, iterations, HashAlgorithmName, OutputLength);
 
         Assert.True(result.SequenceEqual(bytesToVerify));
+    }
+
+
+    [Theory]
+    [InlineData("admin", 100)]
+    [InlineData("admin", 10000)]
+    [InlineData("A1b@C#d$e^", 10000)]
+    [InlineData("A1b@C#d$e^", 100)]
+    [InlineData("A1b@C#d$e^", 1)]
+    public void GivenAHashedPasswordGeneratedWithLegacyMethod_MustBeVerifiedWithNativelyImplemented_CPPHashFunction(string password, int iterations)
+    {
+        byte[] saltBytes;
+        byte[] bytesToVerify;
+
+        using (var algorithm = new Rfc2898DeriveBytes(
+             password,
+             SaltSize,
+             iterations,
+             HashAlgorithmName))
+        {
+            bytesToVerify = algorithm.GetBytes(OutputLength);
+            saltBytes = algorithm.Salt;
+        }
+
+        byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+        // Allocate memory for output
+        byte[] outputArray = new byte[OutputLength];
+
+        NativeCall.Pbkdf2(passwordBytes,
+                (IntPtr)passwordBytes.Length,
+                saltBytes,
+                (IntPtr)saltBytes.Length,
+                (uint)iterations,
+                outputArray,
+                (IntPtr)OutputLength);
+
+
+        Assert.True(bytesToVerify.SequenceEqual(outputArray));
     }
 }
